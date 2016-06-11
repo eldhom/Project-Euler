e0642359e6db03c25e9282fb2bf74aeaaab5d1d3
Albin Vass <wass95@hotmail.se>
2016-06-11 18:35:27 +0200

    Added functionality and fixed bugs

--- 001 Multiples of 3 and 5/main.py
+++ 001 Multiples of 3 and 5/main.py
@@ -2,9 +2,12 @@
 import sys
 import math
 
-def multiples(multiple, maxTest):
+upperBoundary = 1000
+lowerBoundary = 0
+
+def multiples(multiple, upperBoundary, lowerBoundary):
 	count = 0
-	for x in range(0, maxTest):
+	for x in range(lowerBoundary, upperBoundary):
 		if(not x%multiple):
 			count += 1
 
@@ -12,11 +15,30 @@ def multiples(multiple, maxTest):
 
 
 sys.argv.pop(0)
+try:
+	i = sys.argv.index('-u')
+	sys.argv.pop(i)
+	upperBoundary = int(sys.argv.pop(i))
+except:
+	print("Using default upper boundary")
+	
+try:
+	i = sys.argv.index('-l')
+	sys.argv.pop(i)
+	lowerBoundary = int(sys.argv.pop(i))
+	if(lowerBoundary < 0):
+		raise Exception
+except:
+	print("Using default lower boundary")
+
+if(not len(sys.argv)):
+	print("Error: Wrong number of arguments")
+
 for num in sys.argv:
 	try:
 		num = int(num)
 		try:
-			print(multiples(num, 1000) + multiples(num, 1000) - multiples(num, 1000))
+			print(multiples(num, upperBoundary, lowerBoundary) + multiples(num, upperBoundary, lowerBoundary) - multiples(num, upperBoundary, lowerBoundary))
 		except ZeroDivisionError:
 			print(math.nan)
 	except:
