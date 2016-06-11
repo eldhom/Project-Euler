4ceaabf41f889ba81249cc09be45cb2023f2ed6c
Albin Vass <wass95@hotmail.se>
2016-06-11 22:45:51 +0200

    task 006 done

---
+++ 006 sum square distance/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.5
+
+import math
+
+def squareOfSums(num):
+	return math.pow(((num*(num+1))/2),2)
+
+def sumOfSquares(num):
+	return num*(num+1)*(2*num +1)/6
+
+
+x = squareOfSums(20)
+y = sumOfSquares(20)
+print("Difference between square of sums: " + str(x) + " and sum of squares: " + str(y) + " is: " + str(x-y))
