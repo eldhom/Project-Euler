dbd7926fc3ec9abea00360cbab62f13f7a491a19
Albin Vass <wass95@hotmail.se>
2016-06-12 23:44:08 +0200

    Fixed a bug

--- 006 sum square distance/main.py
+++ 006 sum square distance/main.py
@@ -9,6 +9,6 @@ def sumOfSquares(num):
 	return num*(num+1)*(2*num +1)/6
 
 
-x = squareOfSums(20)
-y = sumOfSquares(20)
+x = squareOfSums(100)
+y = sumOfSquares(100)
 print("Difference between square of sums: " + str(x) + " and sum of squares: " + str(y) + " is: " + str(x-y))
