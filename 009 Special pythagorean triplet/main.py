8907b71a87bc605c734314bca918e44c44329eb3
Albin Vass <wass95@hotmail.se>
2016-06-12 14:34:19 +0200

    task 009 done

--- 009 Special pythagorean triplet/main.py
+++ 009 Special pythagorean triplet/main.py
@@ -8,6 +8,6 @@ for c in range(0, 1000):
 			if(a + b +c == 1000):
 				if(math.pow(a, 2) + math.pow(b, 2) == math.pow(c, 2)):
 					print("a+b+c: " + str(a) + "+" + str(b)  + "+" + str(c) + "=" + str(a+b+c))
-					print("a² + b² = c²: " + str(math.pow(a, 2)) + "+" + str(math.pow(b, 2)) + "=" + str(math.pow(c, 2)))
+					print("a²+b²=c²: " + str(math.pow(a, 2)) + "+" + str(math.pow(b, 2)) + "=" + str(math.pow(c, 2)))
 					print("a*b*c:" + str(a) + "*" + str(b) + "*" + str(c) + "=" + str(a*b*c))
 					exit()
