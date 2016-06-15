bfd3e091b601e5d32af6944684aa504ea14c3815
Albin Vass <wass95@hotmail.se>
2016-06-15 11:07:24 +0200

    Fixed task 015, now it gives an int

--- 015 Lattice paths/main.py
+++ 015 Lattice paths/main.py
@@ -3,7 +3,7 @@
 import math
 
 def getNumPaths(n):
-	return math.factorial(2*n)/math.pow(math.factorial(n),2)
+	return int(math.factorial(2*n)/math.pow(math.factorial(n),2))
 	
 
 print(str(getNumPaths(20)))
